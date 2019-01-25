366f170a009b26864b06de7968523be1348229fb
Jon Scheiding <jonscheiding@gmail.com>
2019-01-25 14:26:25 -0500

    Refactored to use arrow keys for selecting rather than entering an item

--- posh-awsp.psm1
+++ posh-awsp.psm1
@@ -30,46 +30,80 @@ function Test-AwsProfileName {
   Throw "Profile $ProfileName doesn't exist.  Run 'aws configure --profile $ProfileName' to create it."
 }
 
-function Read-AwsProfileName {
+function Show-Menu {
   Param(
     [Parameter(Mandatory=$true)]
-    [string[]] $Profiles,
-    [switch] $Quiet
+    [string[]] $Items,
+    [Parameter(Mandatory=$false)]
+    [string] $CurrentItem
   )
 
-  if(!$Quiet) {
-    Write-Host "Which profile do you want to use?"
-    Write-AwsProfilesList -Profiles $Profiles
+  $currentIndex = $Items.IndexOf($currentIndex)
+  if($currentIndex -eq -1) {
+    $currentIndex = 0
   }
 
-  $Response = Read-Host -Prompt "Enter index or profile name"
-  if ($Profiles[$Response]) {
-    return $Profiles[$Response]
+  $cursorTop = [Console]::CursorTop
+  $selectedItem = $null
+
+  for($index = 0; $index -lt $Items.Length; $index++) {
+    $indicator = if($index -eq $currentIndex) { "*" } else { " " }
+    Write-Host "$indicator $index)" $Profiles[$index]
   }
 
-  try {
-    Test-AwsProfileName -Profiles $Profiles -ProfileName $Response
-    return $Response
-  } catch { }
+  while($selectedItem -eq $null) {
+    $moveBy = 0
+    $Response = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+  
+    switch($Response.VirtualKeyCode) {
+      38 { if($currentIndex -gt 0) { $moveBy = -1 } }
+      40 { if($currentIndex -lt $Items.Length - 1) { $moveBy = +1 } }
+      13 { $selectedItem = $Items[$currentIndex] }
+      default {
+        if($Items[$Response.Character]) {
+          $selectedItem = $Items[$Response.Character]
+        }
+      }
+    }
 
-  Write-Error "Invalid entry: $Response"
-  return Read-AwsProfileName -Profiles $Profiles -Quiet
+    if($moveBy -ne 0) {
+      [Console]::SetCursorPosition(0, $cursorTop + $currentIndex)
+      Write-Host -NoNewline " "
+      $currentIndex += $moveBy
+      [Console]::SetCursorPosition(0, $cursorTop + $currentIndex)
+      Write-Host -NoNewline "*"
+      [Console]::SetCursorPosition(0, $cursorTop + $Items.Length)
+    }
+  }
+
+  return $selectedItem
 }
 
-function Write-AwsProfilesList {
+function Read-AwsProfileName {
   Param(
     [Parameter(Mandatory=$true)]
     [string[]] $Profiles
   )
 
-  $CurrentAwsProfile = Get-AwsProfile
+  Write-Host "Select your profile:"
+
+  $currentProfile = Get-AwsProfile
+  $selectedProfile = Show-Menu -Items $Profiles -CurrentItem $currentProfile
+
+  return $selectedProfile
+}
+
+function Write-AwsProfilesList {
+  Param(
+    [Parameter(Mandatory=$true)]
+    [string[]] $Profiles,
+    [Parameter(Mandatory=$false)]
+    [int] $CurrentIndex = 0
+  )
 
   for($index = 0; $index -lt $Profiles.Length; $index++) {
-    Write-Host -NoNewline "$index)" $Profiles[$index]
-    if($Profiles[$index] -eq $CurrentAwsProfile) {
-      Write-Host -NoNewline " (current)"
-    }
-    Write-Host ""
+    $indicator = If($index -eq $CurrentIndex) { "> " } else { "  " }
+    Write-Host $indicator $Profiles[$index]
   }
 }
 
