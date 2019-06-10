4e993da6bf4c45ef50d0b99a3b7003007b5a44a7
Jon Scheiding <jonscheiding@gmail.com>
2019-06-10 16:41:20 -0400

    Function documentation and some other comments

--- posh-awsprofile.psm1
+++ posh-awsprofile.psm1
@@ -1,7 +1,26 @@
 function Get-AWSConfigFile {
+  <#
+    .SYNOPSIS
+      Returns the name of the file to use for AWS CLI configuration profiles.
+
+    .DESCRIPTION
+      If there is a value in the AWS_CONFIG_FILE environment variable, returns
+      that value.  Otherwise, returns the default config file location.
+
+    .LINK
+      https://www.github.com/jonscheiding/posh-awsprofile
+
+    .LINK
+      https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-envvars.html
+  #>
+
   [string]$AwsConfigFile = $Env:AWS_CONFIG_FILE
 
   if([string]::IsNullOrEmpty($AwsConfigFile)) {
+    #
+    # If the environment variable is not configured, calculate
+    # the default location as documented in the AWS CLI docs.
+    #
     [string]$AwsHomeDefault = Join-Path ($Env:HOMEDRIVE + $Env:HOMEPATH) ".aws"
     $AwsConfigFile = Join-Path $AwsHomeDefault "config"
   }
@@ -10,6 +29,21 @@ function Get-AWSConfigFile {
 }
 
 function Get-AWSCurrentProfile {
+  <#
+    .SYNOPSIS
+      Returns the currently set AWS CLI profile, or "default" if none has been specified.
+
+    .DESCRIPTION
+      This returns the value of the AWS_PROFILE environment variable.  If the variable is
+      not set, it will return "default".
+
+      If the variable is set to a value that does not exist as a configured AWS CLI profile,
+      a warning will be displayed.
+
+    .LINK
+      https://www.github.com/jonscheiding/posh-awsprofile
+  #>
+
   $ProfileItem = (Get-Item -ErrorAction Ignore Env:AWS_PROFILE)
   if($null -eq $ProfileItem) {
     $ProfileName = "default"
@@ -23,6 +57,18 @@ function Get-AWSCurrentProfile {
 }
 
 function Set-AWSCurrentProfile {
+  <#
+    .SYNOPSIS
+      Sets the AWS CLI profile to the provided value, or clears it if $null is passed.
+
+    .DESCRIPTION
+      This manipulates the value of the AWS_PROFILE environment variable. If the provided
+      value does not exist as a configured AWS CLI profile, a warning will be displayed.
+
+    .LINK
+      https://www.github.com/jonscheiding/posh-awsprofile
+  #>
+
   Param(
     [Parameter(Mandatory=$true, Position=1)] [AllowEmptyString()]
     [string]$ProfileName
@@ -39,6 +85,18 @@ function Set-AWSCurrentProfile {
 }
 
 function Get-AWSAvailableProfiles {
+  <#
+    .SYNOPSIS
+      Displays the list of available AWS CLI profiles.
+
+    .DESCRIPTION
+      Shows the list of profiles found in the AWS CLI config file, as returned
+      by Get-AWSConfigFile. If that file does not exist, a warning is displayed.
+
+    .LINK
+      https://www.github.com/jonscheiding/posh-awsprofile
+  #>
+
   $AwsConfigFile = Get-AWSConfigFile
 
   if(!(Test-Path $AwsConfigFile -PathType Leaf)) {
@@ -57,6 +115,19 @@ function Get-AWSAvailableProfiles {
 }
 
 function Test-AWSProfile {
+  <#
+    .SYNOPSIS
+      Checks the validity of the provided AWS CLI profile name.
+
+    .DESCRIPTION
+      Checks whether the provided name exists in the list of profiles found
+      in the AWS CLI config file, as returned by Get-AWSConfigFile. If that
+      file does not exist, a warning is displayed.
+
+    .LINK
+      https://www.github.com/jonscheiding/posh-awsprofile
+  #>
+
   Param(
     [Parameter(Mandatory=$true, Position=1)]
     $ProfileName
@@ -73,6 +144,25 @@ function Test-AWSProfile {
 }
 
 function Switch-AWSProfile {
+  <#
+    .SYNOPSIS
+      Displays an interactive menu for choosing an AWS CLI profile.
+
+    .DESCRIPTION
+      Uses the following keys for navigation of the menu.
+        [ Move up the list
+        ] Move down the list
+        \ Set your profile to the currently selected one
+        - Clear your profile
+        = Cancel with no changes
+
+      Additionally, you can press any number key to select the profile
+      indicated by that key.
+
+    .LINK
+      https://www.github.com/jonscheiding/posh-awsprofile
+  #>
+
   $AvailableProfiles = Get-AWSAvailableProfiles
   $CurrentProfile = Get-AWSCurrentProfile
 
@@ -94,6 +184,15 @@ function Switch-AWSProfile {
 }
 
 function Read-MenuSelection {
+  <#
+    .SYNOPSIS
+      Utility function that implements the menu handling for
+      Switch-AWSProfile.
+
+    .LINK
+      https://www.github.com/jonscheiding/posh-awsprofile
+  #>
+
   Param(
     [Parameter(Mandatory = $true)] $Items,
     [Parameter(Mandatory = $true)] $CurrentItem
@@ -103,12 +202,20 @@ function Read-MenuSelection {
   $CurrentIndex = $Items.IndexOf($CurrentItem)
   if($CurrentIndex -lt 0) { $CurrentIndex = 0 }
 
+  #
+  # Initially write out the menu items, including a * indicator
+  # to point out the currently set profile.
+  #
   for($i = 0; $i -lt $Items.Length; $i++) {
     $Indicator = if ($CurrentIndex -eq $i) { "*" } else { " " }
     $Index = if ($i -lt 10) { $i } else { " " }
     Write-Host "$Indicator $Index $($Items[$i])"
   }
 
+  #
+  # Keep track of where the cursor ended up so we can update the
+  # * indicator
+  #
   $CursorTop = [Console]::CursorTop - $Items.Length
 
   while($null -eq $SelectedItem) {
@@ -122,21 +229,17 @@ function Read-MenuSelection {
           $SelectedItem = $Items[$Index]
         }
       }
-      "[" {
-        if ($CurrentIndex -gt 0) { $MoveBy = -1 }
-      }
-      "]" {
-        if ($CurrentIndex -lt $Items.Length - 1) { $MoveBy = 1 }
-      }
-      "\" {
-        $SelectedItem = $Items[$CurrentIndex]
-      }
+      "[" { if ($CurrentIndex -gt 0) { $MoveBy = -1 } }
+      "]" { if ($CurrentIndex -lt $Items.Length - 1) { $MoveBy = 1 } }
+      "\" { $SelectedItem = $Items[$CurrentIndex] }
       "-" { return $null }
       "=" { return 0 }
-      default { Write-Host $_ }
     }
 
     if($MoveBy -ne 0) {
+      #
+      # If [ or ] was pressed, update where the * indicator is shown.
+      #
       [Console]::SetCursorPosition(0, $CursorTop + $CurrentIndex)
       Write-Host -NoNewline " "
       $CurrentIndex += $MoveBy
