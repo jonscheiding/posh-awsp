714df19de5e78778409c4dc65f35bc3c64b536ab
Jon Scheiding <jonscheiding@gmail.com>
2019-06-11 12:22:16 -0400

    Fail build if tests fail to run (e.g. by missing cmdlet)

--- build/run-tests.ps1
+++ build/run-tests.ps1
@@ -1,6 +1,9 @@
-Import-Module Pester
-
 $Result = Invoke-Pester -PassThru
+
+if($null -eq $Result) {
+  throw "No test results were returned."
+}
+
 if($Result.FailedCount -gt 0) {
   throw "$($Result.FailedCount) tests failed."
 }
