70300a4f599ea00cbed87bdbcc58bdedae0101c7
Dennis Roche <dennis.roche@outlook.com>
2020-11-17 20:29:58 +0800

    Fix tests

--- tests/Set-AWSCurrentProfile.Tests.ps1
+++ tests/Set-AWSCurrentProfile.Tests.ps1
@@ -5,19 +5,16 @@ Describe "Set-AWSCurrentProfile" {
   BeforeEach {
     Save-Environment
     Set-Item Env:AWS_PROFILE "some_profile"
-    Set-Item Env:AWS_REGION "some_region"
   }
 
   It "Sets the specified profile when one is provided" {
-    Set-AWSCurrentProfile -ProfileName "some_other_profile" -ProfileRegion "some_other_region"
+    Set-AWSCurrentProfile -ProfileName "some_other_profile"
     $Env:AWS_PROFILE | Should -Be "some_other_profile"
-    $Env:AWS_REGION | Should -Be "some_other_region"
   }
 
   It "Clears the profile when the -Clear parameter is provided" {
     Set-AWSCurrentProfile -Clear
     $Env:AWS_PROFILE | Should -Be $null
-    $Env:AWS_REGION | Should -Be $null
   }
 
   AfterEach {
